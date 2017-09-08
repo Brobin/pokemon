42e294b061a3ca0814ecece966fed830236197a0
Brobin <tobin@bulubox.com>
2017-09-08 14:49:36 -0500

    hotfix

--- pokemon/authentication/mixins.py
+++ pokemon/authentication/mixins.py
@@ -5,5 +5,5 @@ class LoginMixin(object):
 
     def dispatch(self, *args, **kwargs):
         if not self.request.user.is_authenticated():
-            return redirect('login')
+            return redirect('/')
         return super().dispatch(*args, **kwargs)
