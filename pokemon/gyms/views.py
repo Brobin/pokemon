fde474b2f40a38c87fc9b71c33daae3f32500cb9
Brobin <tobin@bulubox.com>
2017-09-29 14:55:05 -0500

    add more stats to the gyms page, add the link to the header

--- pokemon/gyms/views.py
+++ pokemon/gyms/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg, Max, Min
 from django.views.generic import TemplateView
 
 from .models import GymLog
@@ -13,4 +14,24 @@ class GymView(TemplateView):
         context['mystic'] = list(queryset.values_list('mystic', flat=True))
         context['valor'] = list(queryset.values_list('valor', flat=True))
         context['instinct'] = list(queryset.values_list('instinct', flat=True))
+        stats = GymLog.objects.aggregate(
+            mystic_max=Max('mystic'),
+            mystic_min=Min('mystic'),
+            mystic_avg=Avg('mystic'),
+            valor_max=Max('valor'),
+            valor_min=Min('valor'),
+            valor_avg=Avg('valor'),
+            instinct_max=Max('instinct'),
+            instinct_min=Min('instinct'),
+            instinct_avg=Avg('instinct'),
+        )
+        total = (
+            stats['mystic_avg'] +
+            stats['valor_avg'] +
+            stats['instinct_avg']
+        )
+        stats['mystic_pct'] = stats['mystic_avg'] / total * 100
+        stats['valor_pct'] = stats['valor_avg'] / total * 100
+        stats['instinct_pct'] = stats['instinct_avg'] / total * 100
+        context['stats'] = stats
         return context
