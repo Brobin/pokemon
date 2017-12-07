a526cdd152304216c8415b9374b0c451e6b35114
Brobin <tobin@bulubox.com>
2017-12-07 13:49:51 -0600

    gym updates

--- pokemon/gyms/urls.py
+++ pokemon/gyms/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from .views import  GymView, GymLogData
+from .views import  GymView, GymLogData, GymApiView, GymTrainerView
 
 
 urlpatterns = [
+    url(r'^trainers/$', GymTrainerView.as_view(), name='gym-trainers'),
     url(r'^data/$', GymLogData.as_view({'get': 'list'}), name='gym-data'),
+    url(r'^api/$', GymApiView.as_view(), name='gym-api'),
     url(r'^$', GymView.as_view(), name='gyms'),
 ]
--- pokemon/gyms/views.py
+++ pokemon/gyms/views.py
@@ -1,7 +1,12 @@
+import cfscrape
+import datetime
+
 from datetime import timedelta
 
+from django.conf import settings
+from django.http import JsonResponse
 from django.utils import timezone
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 
 from rest_framework.viewsets import ModelViewSet
 
@@ -19,3 +24,43 @@ class GymLogData(ModelViewSet):
 
 class GymView(TemplateView):
     template_name = 'gyms/gyms.html'
+
+
+class GymApiView(View):
+
+    def get(self, request, *args, **kwargs):
+        scraper = cfscrape.create_scraper()
+        scraper.headers.update({'referer': settings.API_REFERER})
+        data = scraper.get(settings.API_URL).json()
+        return JsonResponse(data, safe=False)
+
+
+class GymTrainerView(TemplateView):
+    template_name = 'gyms/trainers.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        scraper = cfscrape.create_scraper()
+        scraper.headers.update({'referer': settings.API_REFERER})
+        data = scraper.get(settings.API_URL).json()
+
+        trainers = {}
+
+        for _gym in data['gyms']:
+            gym = data['gyms'][_gym]
+            for pokemon in gym['pokemon']:
+                name = pokemon['trainer_name']
+                if name not in trainers.keys():
+                    trainers[name] = []
+                trainers[name].append(
+                    [datetime.datetime.fromtimestamp(pokemon['deployment_time']/1000), gym['name']]
+                )
+
+        for key, value in trainers.items():
+            value.sort(key=lambda x: x[0])
+
+        result = [{'username': key, 'gyms': value} for key, value in trainers.items() if len(value) > 1]
+        result.sort(key=lambda x: len(x['gyms']), reverse=True)
+            
+        context['trainers'] = result
+        return context
