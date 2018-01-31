0a67688edc84909a39fc32de2ebc483e5e7cb7d3
Brobin <tobin@bulubox.com>
2018-01-31 13:37:31 -0600

    remove trainer view

--- pokemon/gyms/urls.py
+++ pokemon/gyms/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 
-from .views import  GymView, GymLogData, GymApiView, GymTrainerView
+from .views import GymView, GymLogData, GymApiView
 
 
 urlpatterns = [
-    url(r'^trainers/$', GymTrainerView.as_view(), name='gym-trainers'),
     url(r'^data/$', GymLogData.as_view({'get': 'list'}), name='gym-data'),
     url(r'^api/$', GymApiView.as_view(), name='gym-api'),
     url(r'^$', GymView.as_view(), name='gyms'),
--- pokemon/gyms/views.py
+++ pokemon/gyms/views.py
@@ -44,34 +44,3 @@ class GymApiView(View):
         scraper.headers.update({'referer': settings.API_REFERER})
         data = scraper.get(settings.API_URL).json()
         return JsonResponse(data, safe=False)
-
-
-class GymTrainerView(TemplateView):
-    template_name = 'gyms/trainers.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        scraper = cfscrape.create_scraper()
-        scraper.headers.update({'referer': settings.API_REFERER})
-        data = scraper.get(settings.API_URL).json()
-
-        trainers = {}
-
-        for _gym in data['gyms']:
-            gym = data['gyms'][_gym]
-            for pokemon in gym['pokemon']:
-                name = pokemon['trainer_name']
-                if name not in trainers.keys():
-                    trainers[name] = []
-                trainers[name].append(
-                    [datetime.datetime.fromtimestamp(pokemon['deployment_time']/1000), gym['name']]
-                )
-
-        for key, value in trainers.items():
-            value.sort(key=lambda x: x[0])
-
-        result = [{'username': key, 'gyms': value} for key, value in trainers.items() if len(value) > 1]
-        result.sort(key=lambda x: len(x['gyms']), reverse=True)
-            
-        context['trainers'] = result
-        return context
