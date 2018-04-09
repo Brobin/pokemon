dfee68db4cf0092decf22b57e56b1bc16f16ba13
Brobin <tobin@bulubox.com>
2018-04-09 11:16:32 -0500

    add trainer report

--- pokemon/trainers/urls.py
+++ pokemon/trainers/urls.py
@@ -7,7 +7,8 @@ from .views import (
     TrainerCreate,
     TrainerDetail,
     TrainerEdit,
-    TrainerList
+    TrainerEdit,
+    TrainerReport
 )
 
 
@@ -16,6 +17,7 @@ urlpatterns = [
     url(r'^badges/$', BadgeList.as_view(), name='trainer-badges'),
     url(r'^create/$', TrainerCreate.as_view(), name='trainer-create'),
     url(r'^pokemon/$', PokemonList.as_view(), name='trainer-pokemon'),
+    url(r'^report/$', TrainerReport.as_view(), name='trainer-report'),
     url(r'^(?P<username>.*)/edit/$', TrainerEdit.as_view(), name='trainer-edit'),
     url(r'^(?P<username>.*)/$', TrainerDetail.as_view(), name='trainer-detail'),
     url(r'^$', TrainerList.as_view(), name='trainers'),
--- pokemon/trainers/views.py
+++ pokemon/trainers/views.py
@@ -180,3 +180,13 @@ class TrainerList(LoginMixin, ListView):
             return redirect('trainer-create')
         messages.warning(self.request, 'Sign in to view this page.')
         return redirect('/')
+
+
+class TrainerReport(TemplateView):
+    template_name = 'trainers/report.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['level_40'] = Trainer.objects.filter(xp__gte=20000000).order_by('-xp')
+        context['trainers'] = Trainer.objects.filter(xp__lt=20000000).order_by('-xp')[:25]
+        return context
