c06defef40cb816b271eb25fd01fcf7da18c612f
Brobin <tobin@bulubox.com>
2018-04-30 10:50:58 -0500

    one more spot

--- pokemon/trainers/mixins.py
+++ pokemon/trainers/mixins.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 class LoginMixin(object):
 
     def dispatch(self, *args, **kwargs):
-        if not self.request.user.is_authenticated():
+        if not self.request.user.is_authenticated:
             messages.warning(self.request, 'Sign in to view this page.')
             return redirect('/')
         return super().dispatch(*args, **kwargs)
